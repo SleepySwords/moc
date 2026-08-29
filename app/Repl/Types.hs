0c3cb4dec7d8908e2c93c245b556d7334f2546ab
Ibrahim <33922797+SleepySwords@users.noreply.github.com>
2026-08-29 19:06:55 +1000

    feat: add repl exectuion

--- app/Repl/Types.hs
+++ app/Repl/Types.hs
@@ -1,4 +1,33 @@
 module Repl.Types where
 
-data Expr = Literal String | Array [Expr] | Tuple [Expr] | Function (Expr, Expr) | Call Expr Expr deriving (Show)
-data Statement = Assignment String Expr | Expression Expr deriving (Show)
+import Data.List (intercalate)
+import ModelComputation.FiniteStateAutomota.DFA (DeterministFiniteAutomota)
+import ModelComputation.FiniteStateAutomota.NFA (NondeterministFiniteAutomota)
+import qualified Data.Set as Set
+
+data Expr
+  = Literal String
+  | Ident String
+  | Set (Set.Set Expr)
+  | Tuple [Expr]
+  | Function (Expr, Expr)
+  | Call Expr Expr
+  | NFA NondeterministFiniteAutomota
+  | DFA DeterministFiniteAutomota
+  deriving (Ord, Eq)
+
+data Statement = Assignment String Expr | Expression Expr
+
+instance Show Expr where
+  show (Literal x) = "\"" ++ x ++ "\""
+  show (Set x) = "{ " ++ intercalate ", " (show <$> Set.toList x) ++ " }"
+  show (Tuple x) = "( " ++ intercalate ", " (map show x) ++ " )"
+  show (Function (x, y)) = show x ++ " -> " ++ show y
+  show (Call x y) = show x ++ " " ++ show y
+  show (Ident n) = show n
+  show (NFA n) = show n
+  show (DFA n) = show n
+
+instance Show Statement where
+  show (Assignment x y) = x ++ " := " ++ show y
+  show (Expression x) = show x
